554e8b6c21589784f726a95728ba12f2dd712a8e
Mark Otto <markdotto@gmail.com>
2022-11-12 20:40:27 -0800

    Include JS snippet in color modes page

--- site/assets/js/color-modes/index.js
+++ site/assets/js/color-modes/index.js
@@ -1,13 +1,7 @@
-// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
-// IT'S ALL JUST JUNK FOR OUR DOCS!
-// ++++++++++++++++++++++++++++++++++++++++++
-
 /*!
- * JavaScript for Bootstrap's docs (https://getbootstrap.com/)
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
  * Copyright 2011-2022 The Bootstrap Authors
- * Copyright 2011-2022 Twitter, Inc.
  * Licensed under the Creative Commons Attribution 3.0 Unported License.
- * For details, see https://creativecommons.org/licenses/by/3.0/.
  */
 
 (() => {
